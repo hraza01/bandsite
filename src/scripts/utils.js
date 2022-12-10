05cb71da5c4e8bf830c44d6119bf7c0ba7013022
Hasan Raza <hasan.raza@blackfrosttechnologies.com>
2022-12-10 02:35:13 -0500

    feat: added dynamic content through js

---
+++ src/scripts/utils.js
@@ -0,0 +1,81 @@
+export function addComment(comment) {
+    // create elements needed for a comment
+    const commentBox = document.createElement("article");
+    const commentAvatar = document.createElement("div");
+    const commentDescription = document.createElement("div");
+    const commentName = document.createElement("h4");
+    const commentTimestamp = document.createElement("time");
+    const commentValue = document.createElement("p");
+
+    const firstNode = [commentAvatar, commentDescription];
+    const secondNode = [commentName, commentTimestamp, commentValue];
+
+    // add pre-defined styles for the created elements
+    commentBox.classList.add("cta__comment");
+    commentAvatar.classList.add("cta__comment-avatar");
+    commentDescription.classList.add("cta__comment-description");
+    commentName.classList.add("cta__comment-name");
+    commentTimestamp.classList.add("cta__comment-time");
+    commentValue.classList.add("cta__comment-text");
+
+    // assign comment values
+    commentName.textContent = comment.name;
+    commentTimestamp.textContent = comment.timestamp;
+    commentValue.textContent = comment.value;
+
+    secondNode.forEach((element) => {
+        commentDescription.appendChild(element);
+    });
+
+    firstNode.forEach((element) => {
+        commentBox.appendChild(element);
+    });
+
+    return commentBox;
+}
+
+export function addEvent(event) {
+    // create elements needed for a show
+    const eventBox = document.createElement("div");
+    const eventDateLabel = document.createElement("p");
+    const eventDate = document.createElement("p");
+    const eventVenueLabel = document.createElement("p");
+    const eventVenue = document.createElement("p");
+    const eventLocationLabel = document.createElement("p");
+    const eventLocation = document.createElement("p");
+    const eventBtn = document.createElement("button");
+
+    const labels = [eventDateLabel, eventVenueLabel, eventLocationLabel];
+    const elements = [
+        eventDateLabel,
+        eventDate,
+        eventVenueLabel,
+        eventVenue,
+        eventLocationLabel,
+        eventLocation,
+        eventBtn,
+    ];
+
+    // add pre-defined styles for the created elements
+    labels.forEach((label) => {
+        label.classList.add("shows__event-label");
+    });
+
+    eventBox.classList.add("shows__event");
+    eventDate.classList.add("shows__event-date");
+    eventVenue.classList.add("shows__event-venue");
+    eventLocation.classList.add("shows__event-location");
+    eventBtn.classList.add("shows__btn");
+
+    // assign show values
+    eventDate.textContent = event.date;
+    eventVenue.textContent = event.venue;
+    eventLocation.textContent = event.location;
+    eventBtn.textContent = "buy tickets";
+
+    elements.forEach((value) => {
+        eventBox.appendChild(value);
+    });
+
+    return eventBox;
+}
