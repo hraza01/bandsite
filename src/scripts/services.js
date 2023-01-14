29d32ccdeda79a9e38a5f3e9b6e871378d721adf
Hasan Raza <hasan.raza@blackfrosttechnologies.com>
2023-01-14 14:27:24 +0500

    feat: added like and delete button functionality

--- src/scripts/services.js
+++ src/scripts/services.js
@@ -1,5 +1,33 @@
 "use strict";
 import { createHTMLElement } from "./utils.js";
+import { errorContainer, errorMessage } from "./constants.js";
+import { makeRequest } from "./requests.js";
+
+function displayError() {
+    errorContainer.style.display = "block";
+    errorMessage.innerText = "Error: Invalid Name and/or Comment";
+}
+
+function commentLikeHandler(event) {
+    const likedComment = event.target.closest(".cta__comment-description");
+    const likedCount = likedComment.querySelector(".cta__comment-like-count");
+    const commentId = likedComment.dataset.commentId;
+    makeRequest(`comments/${commentId}/like`, "PUT", null, null)
+        .then((res) => {
+            likedCount.innerText = `${res.likes} Likes`;
+        })
+        .catch((err) => console.error(err.message));
+}
+
+function commentDeleteHandler(event) {
+    const clickedComment = event.target.closest(".cta__comment-description");
+    const commentId = clickedComment.dataset.commentId;
+    makeRequest(`comments/${commentId}`, "DELETE", null, null)
+        .then(() => {
+            clickedComment.parentElement.remove();
+        })
+        .catch((err) => console.error(err.message));
+}
 
 function displayComment(comment) {
     // create elements needed for a comment
@@ -17,15 +45,53 @@ function displayComment(comment) {
     const commentValue = createHTMLElement(
         "p",
         "cta__comment-text",
-        comment.value
+        comment.comment
     );
     const commentDescription = createHTMLElement(
         "div",
         "cta__comment-description"
     );
-    const commentBox = createHTMLElement("div", "cta__comment");
 
-    commentDescription.append(commentName, commentTimestamp, commentValue);
+    const commentInteractivity = createHTMLElement(
+        "div",
+        "cta__comment-interactivity"
+    );
+
+    const commentLikeCount = createHTMLElement(
+        "p",
+        "cta__comment-like-count",
+        `${comment.likes} Likes`
+    );
+
+    const commentLikeBtn = createHTMLElement("a", "cta__comment-like");
+    commentLikeBtn.addEventListener("click", commentLikeHandler);
+
+    // Adding an icon
+    commentLikeBtn.innerHTML =
+        '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">\n' +
+        '  <path stroke-linecap="round" stroke-linejoin="round" d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z" />\n' +
+        "</svg>\n";
+
+    const commentDelBtn = createHTMLElement("a", "cta__comment-delete");
+    commentDelBtn.addEventListener("click", commentDeleteHandler);
+
+    // Adding an icon
+    commentDelBtn.innerHTML =
+        '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">\n' +
+        '  <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />\n' +
+        "</svg>\n";
+
+    const commentBox = createHTMLElement("div", "cta__comment");
+    commentDescription.dataset.commentId = comment.id;
+
+    commentInteractivity.append(commentLikeBtn, commentDelBtn);
+    commentDescription.append(
+        commentName,
+        commentTimestamp,
+        commentValue,
+        commentLikeCount,
+        commentInteractivity
+    );
     commentBox.append(commentAvatar, commentDescription);
 
     return commentBox;
@@ -48,7 +114,7 @@ function displayEvent(event) {
     const eventVenue = createHTMLElement(
         "p",
         "shows__event-venue",
-        event.venue
+        event.place
     );
     const eventLocation = createHTMLElement(
         "p",
@@ -71,4 +137,4 @@ function displayEvent(event) {
     return eventBox;
 }
 
-export { displayComment, displayEvent };
+export { displayComment, displayEvent, displayError };
