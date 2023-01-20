fd9e310d4d952fce106ed3b40fbd727399cbeb5f
Hasan Raza <hasan.raza@blackfrosttechnologies.com>
2023-01-20 11:12:17 +0500

    chore: replaced key

--- src/scripts/auth.js
+++ src/scripts/auth.js
@@ -1,2 +1,1 @@
-// export const API_KEY = "84e8278b-6f4c-4cd1-8553-6a6046e54cb1";
-export const API_KEY = "0128250c-aa8a-48d0-b796-cbc0bccbb45b";
+export const API_KEY = "84e8278b-6f4c-4cd1-8553-6a6046e54cb1";
